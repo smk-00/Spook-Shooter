684226c72710898322b4370ec158042e1a368694
smk <smk11500@gmail.com>
2022-10-28 16:14:42 +0530

    pg init

--- spookShooter.py
+++ spookShooter.py
@@ -1,3 +1,15 @@
 import pygame as pg
 
 pg.init()
+
+
+screen_w = 1240
+screen_h = 680
+
+
+WINDOW = pg.display.set_mode((screen_w,screen_h))
+pg.display.set_caption("Soppky Shooter")
+
+
+
+pg.quit()
